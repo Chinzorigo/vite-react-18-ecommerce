a2bb53262827c2bfa20bc6bb4067aa5d31165be4
Chinzorigo <chinzorig.o@gmail.com>
2023-11-23 00:35:40 +0800

    client side routing - 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./assets/css/lightbox.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import Product from "./components/product";
+import { Link } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -21,27 +22,38 @@ function App() {
   console.log("product: ", products);
 
   return (
-    // <!-- ***** Products Area Starts ***** -->
-    <section className="section" id="products">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="section-heading">
-              <h2>Our Latest Products</h2>
-              <span>Check out all of our products.</span>
+    <>
+      {/* // <-- ***** Products Area Starts ***** --> */}
+      <section className="section" id="products">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-heading">
+                <h2>Our Latest Products</h2>
+                <span>Check out all of our products.</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          {products.map((product) => (
-            <Product product={product} key={product.id} />
-          ))}
+        <div className="container">
+          <div className="row">
+            {products.map((product) => (
+              <Product product={product} key={product.id} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-    //* <!-- ***** Products Area Ends ***** --> *
+      </section>
+      {/* * <!-- ***** Products Area Ends ***** --> * */}
+      <footer>
+        <div className="container">
+          <ul>
+            <li>
+              <Link to="/about">Бидний тухай</Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
 
